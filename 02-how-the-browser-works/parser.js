cdc28de27ae0e38828420cf79958d26dc4f10958
liupeixin <fengyi.mail@gmail.com>
2021-04-23 00:18:45 +0800

    build dom tree

--- 02-how-the-browser-works/parser.js
+++ 02-how-the-browser-works/parser.js
@@ -2,9 +2,42 @@ const EOF = Symbol("EOF");
 
 let currentToken = null;
 let currentAttribute = null;
+const stack = [];
 
 function emit(token) {
-  console.log(token);
+  if (token.type === "text") {
+    return;
+  }
+  let top = stack[stack.length - 1];
+
+  if (token.type === "startTag") {
+    const element = {
+      type: "element",
+      children: [],
+      attributes: [],
+    };
+
+    element.tagName = token.tagName;
+    for (p in token) {
+      if (!["type", "tagName"].includes(p)) {
+        element.attributes.push({ name: p, value: token[p] });
+      }
+    }
+    if(top) {
+      element.parent = top;
+      top.children.push(element);
+    }
+    if(!token.isSelfClosing) {
+      stack.push(element);
+    }
+
+  } else if (token.type === "endTag") {
+    if(top.tagName !== token.tagName) {
+      throw new Error("element not match");
+    } else {
+      stack.pop();
+    }
+  }
 }
 
 function data(c) {
@@ -128,7 +161,7 @@ function afterQuotedAttributeValue(c) {
     return selfClosingStartTag;
   } else if (c === ">") {
     return tagName(c);
-  }  else {
+  } else {
     return beforeAttributeName(c);
   }
 }
@@ -187,4 +220,5 @@ module.exports.parserHTML = function parserHTML(html) {
     state = state(c);
   }
   state = state(EOF);
+  console.log(stack);
 };
